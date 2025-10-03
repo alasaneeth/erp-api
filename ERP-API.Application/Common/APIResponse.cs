e260b650d2ab53fda8f9926b8ffbb4fb0d217093
alasaneeth <alasaneeth22@gmail.com>
2025-10-03 12:31:31 +0300

    Add: Common API Response

---
+++ ERP-API.Application/Common/APIResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_API.Application.Common
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } 
+        public object Result { get; set; }    
+        public string DisaplayMessage { get; set; } = "";
+
+        public List<APIError> Errors { get; set; } = new();
+        public List<APIWarning> Warnings { get; set; } = new();
+
+        public void AddError(string errorMessage)
+        {
+            APIError error = new APIError(description: errorMessage);
+            Errors.Add(error);
+        }
+
+        public void AddWarning(string warningMessage) 
+        {
+            APIWarning warning = new APIWarning(description: warningMessage);
+            Warnings.Add(warning);
+        }
+
+    }
+}
---
+++ ERP-API.Application/Common/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ERP_API.Application.DTO.CustomerDto;
+using ERP_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_API.Application.Common
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CreateCustomerDto>().ReverseMap();
+            CreateMap<Customer, UpdateCustomerDto>().ReverseMap();
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(x => x.CustomerType, opt => opt.MapFrom(source => source.CustomerType.Name));
+        }
+    }
+}
